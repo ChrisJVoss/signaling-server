2579bd6cbfea253bcffa1e344b89864a3a4ae71c
Chris Voss <chrisvoss92@gmail.com>
2017-08-18 10:38:35 -0700

    SSL key and certificate now in environment variables. Simple socket.io server.

---
+++ index.js
@@ -0,0 +1,23 @@
+const https = require('https').createServer({
+  key: process.env.KEY,
+  cert: process.env.CERT
+})
+const io = require('socket.io')(https)
+
+let connections = []
+
+io.on('connection', socket => {
+  connections.push(socket)
+  console.log('%s users connected', connections.length)
+  socket.on('yourId', (id) => {
+    socket.broadcast.emit('otherId', id)
+  })
+  socket.on('disconnect', function() {
+    connections.splice(connections.indexOf(socket), 1)
+    console.log('%s users connected', connections.length)
+  })
+})
+
+http.listen(process.env.PORT, () => {
+  console.log('Listening...')
+})
